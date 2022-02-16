f1d60705966d74687687e43f44d39c07400d3f42
rawnak <eltonrawn@gmail.com>
2022-02-16 16:38:22 +0600

    Refactor to provide better config type safety in rateLimiterService

--- app/ratelimiter/RateLimiterService.scala
+++ app/ratelimiter/RateLimiterService.scala
@@ -1,14 +1,33 @@
 package ratelimiter
 
 import com.typesafe.config.{Config, ConfigFactory}
+import play.api.Logger
 import ratelimiter.implementations.TokenBucket
 
 import javax.inject.Singleton
 
+case class RateLimiterConf(request_limit: Long, period_millis: Long)
+
 @Singleton
 class RateLimiterService {
+  val logger: Logger = Logger(this.getClass())
   val config: Config = ConfigFactory.load()
-  var rateLimiterMap = collection.mutable.Map[String, RateLimiterT]()
+  val rateLimiterMap = collection.mutable.Map[String, RateLimiterT]()
+  val rateLimterDefaultConfig: RateLimiterConf = {
+    val request_limit: Long = {
+      if(config.hasPath("rate-limiter-default-config.request-limit")) {
+        config.getLong("rate-limiter-default-config.request-limit")
+      }
+      else 50
+    }
+    val period_millis: Long = {
+      if(config.hasPath("rate-limiter-default-config.period-millis")) {
+        config.getLong("rate-limiter-default-config.period-millis")
+      }
+      else 10000
+    }
+    RateLimiterConf(request_limit, period_millis)
+  }
 
   def getObject(key: String): RateLimiterT =  {
     if(rateLimiterMap.contains(key)) {
@@ -16,22 +35,30 @@ class RateLimiterService {
     }
     this.synchronized {
       if(!rateLimiterMap.contains(key)) {
-        val rateLimiterConfig: Config = getRateLimiterConfig(key)
-        rateLimiterMap(key) = new TokenBucket(rateLimiterConfig.getLong("request-limit"),
-          rateLimiterConfig.getLong("period-millis"))
+        val rateLimiterConfig: RateLimiterConf = getRateLimiterConfig(key)
+        logger.info("rateLimiterconfig: " + rateLimiterConfig.toString)
+        rateLimiterMap(key) = new TokenBucket(rateLimiterConfig.request_limit, rateLimiterConfig.period_millis)
       }
       return rateLimiterMap(key)
     }
   }
 
-  def getRateLimiterConfig(key: String): Config = {
+  def getRateLimiterConfig(key: String): RateLimiterConf = {
     val exactKey = "rate-limiter-key-value-config.\"" + key + "\""
-    if(config.hasPath(exactKey)) {
-      config.getConfig(exactKey)
+    if(config.hasPath(exactKey)) convertConfigToCase(config.getConfig(exactKey))
+    else rateLimterDefaultConfig
+  }
+
+  def convertConfigToCase(config: Config): RateLimiterConf = {
+    val request_limit: Long = {
+      if(config.hasPath("request-limit")) config.getLong("request-limit")
+      else rateLimterDefaultConfig.request_limit
     }
-    else {
-      config.getConfig("rate-limiter-default-config")
+    val period_millis: Long = {
+      if(config.hasPath("period-millis")) config.getLong("period-millis")
+      else rateLimterDefaultConfig.period_millis
     }
+    RateLimiterConf(request_limit, period_millis)
   }
 
 }
